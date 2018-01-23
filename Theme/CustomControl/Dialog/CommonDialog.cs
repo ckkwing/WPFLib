de00b4415c131344223618664a287dc8bc87977c
Eric <echen@nero.com>
2018-01-23 13:35:09 +0800

    add TabControlStyle

--- Theme/CustomControl/Dialog/CommonDialog.cs
+++ Theme/CustomControl/Dialog/CommonDialog.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Theme.CustomControl.Dialog
 {
     public class CommonDialog : BasicWindow
     {
+        public CommonDialog()
+            :base()
+        {
+            
+        }
+
+        public CommonDialog(Window owner)
+            :base(owner)
+        {
+            
+        }
+
         public enum ResultButtonType
         {
             OK,
