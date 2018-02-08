a4d944820eba9d6e6db9a73a9c13f0432a9421bc
Eric <echen@nero.com>
2018-02-08 15:40:08 +0800

    update

--- Theme/Converters/AutoListViewColumnWidthConverter.cs
+++ Theme/Converters/AutoListViewColumnWidthConverter.cs
@@ -4,8 +4,10 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using Utilities.Extension;
 
 namespace Theme.Converters
 {
@@ -13,9 +15,24 @@ namespace Theme.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            ListView listview = value as ListView;
-            double width = listview.Width;
-            GridView gv = listview.View as GridView;
+            //ListView listview = value as ListView;
+            //double width = listview.Width;
+            //GridView gv = listview.View as GridView;
+            //for (int i = 0; i < gv.Columns.Count; i++)
+            //{
+            //    if (!Double.IsNaN(gv.Columns[i].Width))
+            //        width -= gv.Columns[i].Width;
+            //}
+            //return width - 5;// this is to take care of margin/padding
+
+            ListView listView = value as ListView;
+            double width = double.NaN;
+
+            if (!double.IsNaN(listView.Width))
+                width = listView.Width;
+            else if (!double.IsNaN(listView.ActualWidth))
+                width = listView.ActualWidth;
+            GridView gv = listView.View as GridView;
             for (int i = 0; i < gv.Columns.Count; i++)
             {
                 if (!Double.IsNaN(gv.Columns[i].Width))
@@ -27,6 +44,40 @@ namespace Theme.Converters
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return null;
+        }
+    }
+
+    public class AutoListViewColumnWidthMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double width = double.NaN;
+            if (values[0] == DependencyProperty.UnsetValue || values[1] == DependencyProperty.UnsetValue )
+                return width;
+            if (null == values[0] || null == values[1] )
+                return width;
+
+            try
+            {
+                width = (double)values[0];
+                ListView listView = values[1] as ListView;
+                GridView gv = listView.View as GridView;
+                for (int i = 0; i < gv.Columns.Count; i++)
+                {
+                    if (!Double.IsNaN(gv.Columns[i].Width))
+                        width -= gv.Columns[i].Width;
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+            return width - 5;// this is to take care of margin/padding
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Theme/Helper/ListViewStyleHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Theme.Helper
+{
+    public static class ListViewStyleHelper
+    {
+        #region Sort Default Style
+        /// <summary>
+        /// View sort style, deafult is null
+        /// </summary>
+        public static readonly DependencyProperty SortDefaultStyleProperty =
+            DependencyProperty.RegisterAttached("SortDefaultStyle", typeof(DataTemplate), typeof(ListViewStyleHelper));
+
+        public static void SetSortDefaultStyle(DependencyObject element, DataTemplate value)
+        {
+            if (element == null) return;
+            element.SetValue(SortDefaultStyleProperty, value);
+        }
+
+        public static DataTemplate GetSortDefaultStyle(DependencyObject element)
+        {
+            if (element == null) return null;
+            return element.GetValue(SortDefaultStyleProperty) as DataTemplate;
+        }
+        #endregion
+
+        #region sort Asc Style
+        /// <summary>
+        /// view sort style ,asc arrow
+        /// </summary>
+        public static readonly DependencyProperty SortAscStyleProperty =
+                   DependencyProperty.RegisterAttached("SortAscStyle", typeof(DataTemplate), typeof(ListViewStyleHelper));
+
+        public static void SetSortAscStyle(DependencyObject element, DataTemplate value)
+        {
+            if (element == null) return;
+            element.SetValue(SortAscStyleProperty, value);
+        }
+
+        public static DataTemplate GetSortAscStyle(DependencyObject element)
+        {
+            if (element == null) return null;
+            return element.GetValue(SortAscStyleProperty) as DataTemplate;
+        }
+        #endregion
+
+        #region sort Desc Style
+        /// <summary>
+        /// view sort style, desc arrow
+        /// </summary>
+        public static readonly DependencyProperty SortDescStyleProperty =
+                   DependencyProperty.RegisterAttached("SortDescStyle", typeof(DataTemplate), typeof(ListViewStyleHelper));
+
+        public static void SetSortDescStyle(DependencyObject element, DataTemplate value)
+        {
+            if (element == null) return;
+            element.SetValue(SortDescStyleProperty, value);
+        }
+
+        public static DataTemplate GetSortDescStyle(DependencyObject element)
+        {
+            if (element == null) return null;
+            return element.GetValue(SortDescStyleProperty) as DataTemplate;
+        }
+        #endregion
+    }
+}
