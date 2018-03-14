b4c10d0f0ec2bb7ad7452b03406697ef6090b1e1
Eric <echen@nero.com>
2018-03-14 15:33:02 +0800

    UPDATE

---
+++ Theme/Converters/BoolToVisibilityCollapseConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Theme.Converters
+{
+    public class BoolToVisibilityCollapseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+            {
+                return null;
+            }
+            bool flag = (bool)value;
+            if (this.IsInverse)
+            {
+                return (flag ? Visibility.Collapsed : Visibility.Visible);
+            }
+            return (flag ? Visibility.Visible : Visibility.Collapsed);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsInverse { get; set; }
+
+    }
+}
---
+++ Theme/Converters/ObjectToBoolConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Theme.Converters
+{
+    public class ObjectToBoolConverter : IValueConverter
+    {
+        public bool IsInverse { get; set; }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool bRel = false;
+            try
+            {
+                if (value == DependencyProperty.UnsetValue)
+                {
+                    return false;
+                }
+                if (null == value)
+                    return false;
+                if (value is string)
+                {
+                    string str = value as string;
+                    if (string.IsNullOrEmpty(str))
+                        return false;
+                }
+
+                bRel = true;
+            }
+            catch
+            {
+            }
+            if (this.IsInverse)
+                return !bRel;
+            return bRel;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
