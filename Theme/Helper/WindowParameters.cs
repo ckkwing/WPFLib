13e43ffe0812337b936ccbd55fd557983644d104
Eric Chen <kankan.chen@gmail.com>
2024-03-27 16:29:21 +0800

    update

---
+++ Theme/Helper/WindowParameters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Theme.Native;
+
+namespace Theme.Helper
+{
+    public class WindowParameters
+    {
+        private static Thickness _paddedBorderThickness;
+        /// <summary>
+        /// returns the border thickness padding around captioned windows,in pixels. Windows XP/2000:  This value is not supported.
+        /// </summary>
+        public static Thickness PaddedBorderThickness
+        {
+            [SecurityCritical]
+            get
+            {
+                if (_paddedBorderThickness == null)
+                {
+                    var paddedBorder = NativeMethods.GetSystemMetrics(NativeMethods.SM_CXPADDEDBORDER);
+                    //var dpi = GetDpi();
+                    Size frameSize = new Size(paddedBorder, paddedBorder);
+                    Size frameSizeInDips = DpiHelper.DeviceSizeToLogical(frameSize);
+                    _paddedBorderThickness = new Thickness(frameSizeInDips.Width, frameSizeInDips.Height, frameSizeInDips.Width, frameSizeInDips.Height);
+                }
+
+                return _paddedBorderThickness;
+            }
+        }
+    }
+}
--- Theme/Native/NativeMethods.cs
+++ Theme/Native/NativeMethods.cs
@@ -1,27 +1,223 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Theme.Helper;
 
 namespace Theme.Native
 {
-    internal static class NativeMethods
+    public static class NativeMethods
     {
+        public const int SM_CXPADDEDBORDER = 4;
+
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool IsWindowEnabled(IntPtr hWnd);
+        public static extern bool IsWindowEnabled(IntPtr hWnd);
 
         [DllImport("user32.dll", ExactSpelling = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SetForegroundWindow(IntPtr hWnd);
 
         [DllImport("user32.dll")]
-        internal static extern IntPtr SetParent(IntPtr hWnd, IntPtr hWndParent);
+        public static extern IntPtr SetParent(IntPtr hWnd, IntPtr hWndParent);
 
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool EnableWindow(IntPtr hWnd, [MarshalAs(UnmanagedType.Bool)] bool value);
+        public static extern bool EnableWindow(IntPtr hWnd, [MarshalAs(UnmanagedType.Bool)] bool value);
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        [DllImport("gdi32.dll")]
+        public static extern int GetDeviceCaps(SafeDC hdc, DeviceCap nIndex);
+
+        public sealed class SafeDC : SafeHandleZeroOrMinusOneIsInvalid
+        {
+            private static class NativeMethods
+            {
+                [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+                [DllImport("user32.dll")]
+                public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+                [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+                [DllImport("user32.dll")]
+                public static extern SafeDC GetDC(IntPtr hwnd);
+
+                // Weird legacy function, documentation is unclear about how to use it...
+                [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+                [DllImport("gdi32.dll", CharSet = CharSet.Unicode)]
+                public static extern SafeDC CreateDC([MarshalAs(UnmanagedType.LPWStr)] string lpszDriver, [MarshalAs(UnmanagedType.LPWStr)] string lpszDevice, IntPtr lpszOutput, IntPtr lpInitData);
+
+                [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+                [DllImport("gdi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+                public static extern SafeDC CreateCompatibleDC(IntPtr hdc);
+
+                [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+                [DllImport("gdi32.dll")]
+                [return: MarshalAs(UnmanagedType.Bool)]
+                public static extern bool DeleteDC(IntPtr hdc);
+            }
+
+            private IntPtr? _hwnd;
+            private bool _created;
+
+            public IntPtr Hwnd
+            {
+                set
+                {
+                    Assert.NullableIsNull(_hwnd);
+                    _hwnd = value;
+                }
+            }
+
+            private SafeDC() : base(true) { }
+
+            [ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+            protected override bool ReleaseHandle()
+            {
+                if (_created)
+                {
+                    return NativeMethods.DeleteDC(handle);
+                }
+
+                if (!_hwnd.HasValue || _hwnd.Value == IntPtr.Zero)
+                {
+                    return true;
+                }
+
+                return NativeMethods.ReleaseDC(_hwnd.Value, handle) == 1;
+            }
+
+            [SuppressMessage("Microsoft.Usage", "CA2201:DoNotRaiseReservedExceptionTypes"), SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+            public static SafeDC CreateDC(string deviceName)
+            {
+                SafeDC dc = null;
+                try
+                {
+                    // Should this really be on the driver parameter?
+                    dc = NativeMethods.CreateDC(deviceName, null, IntPtr.Zero, IntPtr.Zero);
+                }
+                finally
+                {
+                    if (dc != null)
+                    {
+                        dc._created = true;
+                    }
+                }
+
+                if (dc.IsInvalid)
+                {
+                    dc.Dispose();
+                    throw new SystemException("Unable to create a device context from the specified device information.");
+                }
+
+                return dc;
+            }
+
+            [SuppressMessage("Microsoft.Usage", "CA2201:DoNotRaiseReservedExceptionTypes"), SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+            public static SafeDC CreateCompatibleDC(SafeDC hdc)
+            {
+                SafeDC dc = null;
+                try
+                {
+                    IntPtr hPtr = IntPtr.Zero;
+                    if (hdc != null)
+                    {
+                        hPtr = hdc.handle;
+                    }
+                    dc = NativeMethods.CreateCompatibleDC(hPtr);
+                    if (dc == null)
+                    {
+                        HRESULT.ThrowLastError();
+                    }
+                }
+                finally
+                {
+                    if (dc != null)
+                    {
+                        dc._created = true;
+                    }
+                }
+
+                if (dc.IsInvalid)
+                {
+                    dc.Dispose();
+                    throw new SystemException("Unable to create a device context from the specified device information.");
+                }
+
+                return dc;
+            }
+
+            public static SafeDC GetDC(IntPtr hwnd)
+            {
+                SafeDC dc = null;
+                try
+                {
+                    dc = NativeMethods.GetDC(hwnd);
+                }
+                finally
+                {
+                    if (dc != null)
+                    {
+                        dc.Hwnd = hwnd;
+                    }
+                }
+
+                if (dc.IsInvalid)
+                {
+                    // GetDC does not set the last error...
+                    HRESULT.E_FAIL.ThrowIfFailed();
+                }
+
+                return dc;
+            }
+
+            public static SafeDC GetDesktop()
+            {
+                return GetDC(IntPtr.Zero);
+            }
+
+            [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+            [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+            public static SafeDC WrapDC(IntPtr hdc)
+            {
+                // This won't actually get released by the class, but it allows an IntPtr to be converted for signatures.
+                return new SafeDC
+                {
+                    handle = hdc,
+                    _created = false,
+                    _hwnd = IntPtr.Zero,
+                };
+            }
+        }
+
+        /// <summary>
+        /// GetDeviceCaps nIndex values.
+        /// </summary>
+        public enum DeviceCap
+        {
+            /// <summary>Number of bits per pixel
+            /// </summary>
+            BITSPIXEL = 12,
+            /// <summary>
+            /// Number of planes
+            /// </summary>
+            PLANES = 14,
+            /// <summary>
+            /// Logical pixels inch in X
+            /// </summary>
+            LOGPIXELSX = 88,
+            /// <summary>
+            /// Logical pixels inch in Y
+            /// </summary>
+            LOGPIXELSY = 90,
+        }
+
     }
 }
