7067a3f4fb0f683a10913bb4a6dd8a1adfb3fa75
Eric Chen <echen@nero.com>
2025-03-25 15:31:51 +0800

    Update

---
+++ Theme/Attributes/LocalizableDescriptionAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theme.Resources;
+
+namespace Theme.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+    public sealed class LocalizableDescriptionAttribute : DescriptionAttribute
+    {
+        private readonly string id;
+
+        /// <summary>
+        /// A <see cref="DescriptionAttribute"/> loading the description from the applications ressources.
+        /// </summary>
+        /// <param name="id">The string id of the localized string.</param>
+        public LocalizableDescriptionAttribute(string id)
+            : base(id)
+        {
+            this.id = id;
+        }
+
+        /// <summary>
+        /// Get the string value from the resources.
+        /// Always lookup the resource so we don't miss language changes. (TODO: cache this and register for language changes?)
+        /// </summary>
+        /// <returns>The localized description.</returns>
+        public override string Description
+        {
+            get
+            {
+                return ResourceProvider.LoadString(Id);
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+    }
+}
---
+++ Theme/Events/DataRoutedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Theme.Events
+{
+    public class DataRoutedEventArgs : RoutedEventArgs
+    {
+        public object Data { get; set; }
+        public DataRoutedEventArgs(RoutedEvent routedEvent, object source)
+            : base(routedEvent, source)
+        {
+        }
+    }
+}
