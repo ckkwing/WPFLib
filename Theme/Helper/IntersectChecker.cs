55de0382eb1cff1c653fcd5adbaad55e163096f3
Eric Chen <echen@nero.com>
2025-09-25 15:15:20 +0800

    Update

---
+++ Theme/Helper/IntersectChecker.cs
@@ -0,0 +1,98 @@
+﻿using CommonUtility.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Theme.Helper
+{
+    /// <summary>
+    /// A static class to check if two objects intersect or not.
+    /// </summary>
+    /// <remarks>
+    /// This class is not fully tested. Use with caution.
+    ///</remarks>
+    [Warning("This class is not fully tested. Use with caution.")]
+    public static class IntersectChecker
+    {
+        /// <summary>
+        /// 使用Intersect方法判断是否有交集
+        /// </summary>
+        public static bool AreRectanglesDisjointUsingIntersect(Rect rect1, Rect rect2)
+        {
+            Rect intersection = Rect.Intersect(rect1, rect2);
+
+            // 如果交集矩形是空的（Width或Height为0或负数），说明没有实际交集
+            return intersection.IsEmpty || intersection.Width <= 0 || intersection.Height <= 0;
+        }
+
+        /// <summary>
+        /// 判断两个矩形是否没有交集
+        /// </summary>
+        /// <param name="rect1">第一个矩形</param>
+        /// <param name="rect2">第二个矩形</param>
+        /// <returns>如果没有交集返回true，否则返回false</returns>
+        public static bool AreRectanglesDisjoint(Rect rect1, Rect rect2)
+        {
+            return !rect1.IntersectsWith(rect2);
+        }
+
+        /// <summary>
+        /// 判断多个矩形是否两两之间都没有交集
+        /// </summary>
+        public static bool AreAllRectanglesDisjoint(params Rect[] rectangles)
+        {
+            for (int i = 0; i < rectangles.Length; i++)
+            {
+                for (int j = i + 1; j < rectangles.Length; j++)
+                {
+                    if (rectangles[i].IntersectsWith(rectangles[j]))
+                    {
+                        return false; // 发现交集
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 获取两个矩形的交集区域（如果没有交集返回空矩形）
+        /// </summary>
+        public static Rect GetIntersectionArea(Rect rect1, Rect rect2)
+        {
+            Rect intersection = Rect.Intersect(rect1, rect2);
+
+            if (intersection.IsEmpty || intersection.Width <= 0 || intersection.Height <= 0)
+            {
+                return Rect.Empty;
+            }
+
+            return intersection;
+        }
+
+        /// <summary>
+        /// 检测两个矩形是否有交集，并且交集面积占第一个矩形面积的百分比达到指定值
+        /// </summary>
+        /// <param name="rectToCalculateIntersection">第一个矩形</param>
+        /// <param name="rectToCompare">第二个矩形</param>
+        /// <param name="percent">指定的百分比，默认值为50%</param>
+        /// <returns>如果交集面积占第一个矩形面积的百分比达到指定值返回true，否则返回false</returns>
+        public static bool HasIntersectionByPercent(Rect rectToCalculateIntersection, Rect rectToCompare, double percent = 0.5)
+        {
+            Rect intersection = Rect.Intersect(rectToCalculateIntersection, rectToCompare);
+
+            if (intersection.IsEmpty)
+                return false;
+
+            double areaToCalculateIntersection = rectToCalculateIntersection.Width * rectToCalculateIntersection.Height;
+            if (areaToCalculateIntersection == 0)
+                return false;
+
+            double areaIntersection = intersection.Width * intersection.Height;
+            return (areaIntersection / areaToCalculateIntersection) >= percent;
+        }
+
+    }
+}
---
+++ Theme/Helper/ScreenMonitor.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using Theme.ViewModels;
+using System.Drawing;
+
+namespace Theme.Helper
+{
+    /// <summary>
+    /// A simplified screen detection class that supports detecting horizontally arranged multiple screens, unit is pixel
+    /// </summary>
+    public class ScreenMonitor : NotifyPropertyObject, IDisposable
+    {
+        public Screen PrimaryScreen
+        {
+            get => Screen.PrimaryScreen;
+        }
+
+        private IList<Screen> _sortedScreens;
+        public IList<Screen> SortedScreens
+        {
+            get
+            {
+                if (null == _sortedScreens)
+                {
+                    _sortedScreens = Screen.AllScreens.OrderBy(s => s.Bounds.X).ToList();
+                }
+                return _sortedScreens;
+            }
+        }
+
+        private Rectangle _workingArea = new Rectangle();
+        public Rectangle WorkingArea
+        {
+            get
+            {
+                if (_workingArea.IsEmpty)
+                {
+                    int? x = null;
+                    int? y = null;
+                    int width = 0;
+                    int height = 0;
+                    foreach (Screen screen in SortedScreens)
+                    {
+                        if (!x.HasValue)
+                            x = screen.WorkingArea.X;
+                        if (!y.HasValue)
+                            y = screen.WorkingArea.Y;
+                        width += screen.WorkingArea.Width;
+                        height += screen.WorkingArea.Height;
+                    }
+                    _workingArea = new Rectangle(x.Value, y.Value, width, height);
+                }
+                return _workingArea;
+            }
+        }
+
+        private Rectangle _screenArea = new Rectangle();
+        public Rectangle ScreenArea
+        {
+            get
+            {
+                if (_screenArea.IsEmpty)
+                {
+                    int? x = null;
+                    int? y = null;
+                    int width = 0;
+                    int height = 0;
+                    foreach (Screen screen in SortedScreens)
+                    {
+                        if (!x.HasValue)
+                            x = screen.Bounds.X;
+                        if (!y.HasValue)
+                            y = screen.Bounds.Y;
+                        width += screen.Bounds.Width;
+                        height += screen.Bounds.Height;
+                    }
+                    _screenArea = new Rectangle(x.Value, y.Value, width, height);
+                }
+                return _screenArea;
+            }
+        }
+
+        public ScreenMonitor()
+        {
+        }
+
+        public void Detect()
+        {
+            _sortedScreens = null;
+            _workingArea = new Rectangle();
+            _screenArea = new Rectangle();
+        }
+
+        public bool IsPointOnAnyScreen(Point point)
+        {
+            return Screen.AllScreens.Any(screen => screen.Bounds.Contains(point));
+        }
+
+        public bool IsPointOnAnyWorkingArea(Point point)
+        {
+            return Screen.AllScreens.Any(screen => screen.WorkingArea.Contains(point));
+        }
+
+        public bool AreDisjoint(System.Windows.Rect rect1, System.Windows.Rect rect2)
+        {
+            return !rect1.IntersectsWith(rect2);
+        }
+
+        public bool IsDisjointScreen(System.Windows.Rect rect)
+        {
+            return AreDisjoint(rect, RectangleToRect(ScreenArea));
+        }
+
+        public bool IsDisjointWorkingArea(System.Windows.Rect rect)
+        {
+            return AreDisjoint(rect, RectangleToRect(WorkingArea));
+        }
+
+        private System.Windows.Rect RectangleToRect(Rectangle rectangle)
+        {
+            return new System.Windows.Rect(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+        }
+
+        public bool ContentHasIntersectionWithScreenByPercent(System.Windows.Rect rect, double percent = 0.5)
+        {
+            return IntersectChecker.HasIntersectionByPercent(rect, RectangleToRect(ScreenArea), percent);
+        }
+
+        public bool ContentHasIntersectionWithWorkingAreaByPercent(System.Windows.Rect rect, double percent = 0.5)
+        {
+            return IntersectChecker.HasIntersectionByPercent(rect, RectangleToRect(WorkingArea), percent);
+        }
+
+        #region Static functions
+
+        public static Screen GetCurrentScreen(System.Windows.Window window)
+        {
+            var windowInteropHelper = new WindowInteropHelper(window);
+            IntPtr handle = windowInteropHelper.Handle;
+
+            return Screen.FromHandle(handle);
+        }
+
+        public static Dictionary<Screen, string> GetMonitorPositions()
+        {
+            var result = new Dictionary<Screen, string>();
+            var screens = Screen.AllScreens;
+
+            if (screens.Length == 1)
+            {
+                result.Add(screens[0], "单一显示器");
+                return result;
+            }
+
+            // 按X坐标排序显示器
+            var sortedScreens = screens.OrderBy(s => s.Bounds.X).ToArray();
+
+            for (int i = 0; i < sortedScreens.Length; i++)
+            {
+                string position;
+
+                if (i == 0)
+                {
+                    position = "最左侧显示器";
+                }
+                else if (i == sortedScreens.Length - 1)
+                {
+                    position = "最右侧显示器";
+                }
+                else
+                {
+                    position = $"中间显示器 ({i + 1}/{sortedScreens.Length})";
+                }
+
+                result.Add(sortedScreens[i], position);
+            }
+
+            return result;
+        }
+
+        public static Dictionary<Screen, string> GetMonitorPositionsRelativeToPrimary()
+        {
+            var result = new Dictionary<Screen, string>();
+            var screens = Screen.AllScreens;
+            var primaryScreen = Screen.PrimaryScreen;
+
+            foreach (var screen in screens)
+            {
+                if (screen.Equals(primaryScreen))
+                {
+                    result.Add(screen, "主显示器");
+                    continue;
+                }
+
+                // 判断相对位置
+                if (screen.Bounds.X < primaryScreen.Bounds.X)
+                {
+                    result.Add(screen, "位于主显示器左侧");
+                }
+                else if (screen.Bounds.X > primaryScreen.Bounds.X)
+                {
+                    result.Add(screen, "位于主显示器右侧");
+                }
+                else
+                {
+                    // X坐标相同，检查Y坐标
+                    if (screen.Bounds.Y < primaryScreen.Bounds.Y)
+                    {
+                        result.Add(screen, "位于主显示器上方");
+                    }
+                    else if (screen.Bounds.Y > primaryScreen.Bounds.Y)
+                    {
+                        result.Add(screen, "位于主显示器下方");
+                    }
+                    else
+                    {
+                        result.Add(screen, "与主显示器位置重叠");
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        protected bool _alreadyDisposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (_alreadyDisposed)
+                return;
+            if (isDisposing)
+            {
+                //release managed resources
+            }
+            //release unmanaged resources
+            _alreadyDisposed = true;
+        }
+
+        ~ScreenMonitor()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+    }
+}
